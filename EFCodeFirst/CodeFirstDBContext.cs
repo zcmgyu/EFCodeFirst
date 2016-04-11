acc7633062ad8f7b9f03245b304c71a95f413012
Long Nguyen <zcmgyu@gmail.com>
2016-04-11 18:52:40 +0700

    init class DB Context

---
+++ EFCodeFirst/CodeFirstDBContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirst
+{
+    public class Standard
+    {
+        public int StandardId { get; set; }
+        public string StandardName { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Student> Students  { get; set; }
+
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int? StandardId { get; set; }
+        public string Address { get; set; }
+        public virtual Standard Standard{ get; set; }
+
+    }
+
+    class CodeFirstDbContext:DbContext
+    {
+
+        public CodeFirstDbContext() : base("name=SchoolDB")
+        {
+            
+        }
+    }
+}
