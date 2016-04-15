827c64f0c2746e3c63f83fd548cf6f78b796ac4c
Kieu Bao Duy <kieubaoduy1412@gmail.com>
2016-04-15 18:48:41 +0700

    Duy edit

--- EFCodeFirst/Program.cs
+++ EFCodeFirst/Program.cs
@@ -17,6 +17,7 @@ namespace EFCodeFirst
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // Duy edit
         }
     }
 }
